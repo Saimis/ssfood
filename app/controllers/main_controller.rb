fcf294081ae31e0dde0b43046274172ba8d20ac1
saimis <saimis@saimis-pc.(none)>
2014-02-03 19:38:01 +0200

    still trying to fix postgres

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -33,7 +33,7 @@ class MainController < ApplicationController
 
     if !current_round.restaurant_id.nil? && !current_user.nil?
        food_history = Userarchyves.joins(:archyves)
-        .where('"userarchyves"."food" NOT NULL AND "userarchyves"."user_id" = ? AND "archyves"."restaurant_id" == ?', current_user.id.to_s, current_round.restaurant_id.to_s)
+        .where('"userarchyves"."food" NOT NULL AND "userarchyves"."user_id" = 3 AND "archyves"."restaurant_id" = 6')
     end
     
     # return json 
